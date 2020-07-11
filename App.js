6bbabd26de92fcbeb1966832b9255345d714ec66
hisami <koron.take@gmail.com>
2020-07-11 11:37:38 +0900

    fix

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app</Text>
+      <Button
+        title="click me"
+        onPress={() => {
+          alert('alert test')
+          console.log('console test')
+        }}
+      />
     </View>
   );
 }
